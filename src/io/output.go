53e94da9621111fcdab24e272416d9df4f05cfa7
mocoma <mocomaminecraft@gmail.com>
2020-04-21 00:34:26 +0200

    I forgot to add everything, oops

--- src/io/output.go
+++ src/io/output.go
@@ -2,15 +2,11 @@ package io
 
 import "fmt"
 
-func Print(s string) {
-   fmt.Println(s)
-}
-
 func AskUser(what string) {
    fmt.Printf("Please enter %s: ", what)
 }
 
-func UserMistake(message string) {
+func ReportUserMistake(message string) {
    fmt.Printf("Oops! it seems you made a mistake: %s\n", message)
    fmt.Printf("Please try again: ")
 }
